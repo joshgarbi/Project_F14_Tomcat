170941328a4805fb3593c5d46bed99e719e8df0f
Josh Garbi <joshua.garbi@gmail.com>
2025-03-30 16:40:22 -0700

    working dcs bios

---
+++ Modules/RIO/VUHF_Radio/TEST_DISPLAY_BIOS.ino
@@ -0,0 +1,43 @@
+/*
+  Tell DCS-BIOS to use a serial connection and use the default Arduino Serial
+  library. This will work on the vast majority of Arduino-compatible boards,
+  but you can get corrupted data if you have too many or too slow outputs
+  (e.g. when you have multiple character displays), because the receive
+  buffer can fill up if the sketch spends too much time updating them.
+  
+  If you can, use the IRQ Serial connection instead.
+*/
+#define DCSBIOS_DEFAULT_SERIAL
+
+#include "DcsBios.h"
+#include <max7219.h>
+
+MAX7219 max7219;
+
+void display(String text, int dec) {
+    max7219.DisplayChar(0, text[0], 0);
+    max7219.DisplayChar(1, text[1], 0);
+    max7219.DisplayChar(2, text[2], dec);
+    max7219.DisplayChar(3, text[3], 0);
+    max7219.DisplayChar(4, text[4], 0);
+    max7219.DisplayChar(5, text[5], 0);
+}
+
+void onVuhfFreqChange(char* newValue) {
+    /* your code here */
+    String freq = String(newValue);
+    freq.remove(3, 1);
+    display(freq, 1);
+}
+DcsBios::StringBuffer<7> vuhfFreqBuffer(0x14e4, onVuhfFreqChange);
+
+void setup() {
+    DcsBios::setup();
+    max7219.Clear();
+    max7219.Begin();
+}
+
+void loop() {
+    DcsBios::loop();
+}
+
