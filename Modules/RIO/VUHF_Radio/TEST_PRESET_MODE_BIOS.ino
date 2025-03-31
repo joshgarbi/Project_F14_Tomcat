7fee756f1e6d7e7d68160f6927d45f272592af1c
Joshua Garbi <joshua.garbi@wallawalla.edu>
2025-03-31 14:04:12 -0700

    adding files

---
+++ Modules/RIO/VUHF_Radio/TEST_PRESET_MODE_BIOS.ino
@@ -0,0 +1,26 @@
+/*
+  Tell DCS-BIOS to use a serial connection and use the default Arduino Serial
+  library. This will work on the vast majority of Arduino-compatible boards,
+  but you can get corrupted data if you have too many or too slow outputs
+  (e.g. when you have multiple character displays), because the receive
+  buffer can fill up if the sketch spends too much time updating them.
+  
+  If you can, use the IRQ Serial connection instead.
+*/
+#define DCSBIOS_DEFAULT_SERIAL
+
+const int APin = 4;
+const int BPin = 5;
+
+#include "DcsBios.h"
+
+DCSBios::RotaryEnoder rioVuhfPresets("RIO_VUHF_PRESETS", "DEC", "INC", APin, BPin);
+
+void setup() {
+  DcsBios::setup();
+}
+
+void loop() {
+  DcsBios::loop();
+}
+
